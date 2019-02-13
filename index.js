76ab4637e154841b968d594fbc1cc6f3a7ea5a05
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-13 02:48:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, mexperience) {
+  this.name = name;
+  this.age = age;
+  this.experience = mexperience;
+}
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
